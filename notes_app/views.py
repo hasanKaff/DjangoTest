24f9df7588def0997ec0a89d60bcd48719d8a05f
Hasan Salem Alkaf <hasan.s.alkaf@kfupm.edu.sa>
2020-10-13 14:21:45 +0300

    Commit-Views

--- notes_app/views.py
+++ notes_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+from .models import Note
+
+
+def all_notes(request):
+    #html = "<html> <body> <h1>Welcome In Django with Hasan Alkaf </h1> </body> </html>"
+    #return HttpResponse(html)
+    #render(request, '<h1>Welcome In Django with Hasan Alkaf </h1>', {})
+    all_notes = Note.objects.all()
+
+    context ={
+        'all_notes' : all_notes
+    }
+
+    return render(request, 'all_notes.html', context)
+
+def detail(request, slug):
+    note = Note.objects.get(slug=slug)
+    context = {
+    'note' : note
+    }
+    return render(request, 'note_detail.html' , context)
