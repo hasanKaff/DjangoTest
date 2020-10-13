48b7f4d0730e90fb0a805811bd1c0fbe9e752bb9
Hasan Salem Alkaf <hasan.s.alkaf@kfupm.edu.sa>
2020-10-13 10:33:45 +0300

    Note-submit

--- notes_app/models.py
+++ notes_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 import datetime
 # Create your models here.
-class Notes(models.Model):
+class Note(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     slug = models.SlugField()
@@ -10,3 +10,6 @@ class Notes(models.Model):
     created = models.DateTimeField(blank=True, default=datetime.datetime.now)
     active = models.BooleanField(default=True)
     tags = models.CharField(blank=True, max_length=100)
+
+def __str__(self):
+    return self.slug
