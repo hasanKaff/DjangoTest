68218d4fc88a04146075d982dc9ba18604abd3ff
Hasan Salem Alkaf <hasan.s.alkaf@kfupm.edu.sa>
2020-10-13 10:12:05 +0300

    migrations-commit

--- notes_app/models.py
+++ notes_app/models.py
@@ -1,5 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import datetime
 # Create your models here.
 class Notes(models.Model):
-    user = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    slug = models.SlugField()
+    content = models.TextField(blank=True)
+    created = models.DateTimeField(blank=True, default=datetime.datetime.now)
+    active = models.BooleanField(default=True)
+    tags = models.CharField(blank=True, max_length=100)
